bcc5d83df1c2290e9e6e0318248887a9dfc43e33
Stephanie Clark <sclark@inventoryshield.com>
2022-06-02 17:46:43 -0600

    update imgOptions

--- lesson7/script.js
+++ lesson7/script.js
@@ -5,7 +5,10 @@ const loadImages = (image) => {
     image.removeAttribute("data-src");
   };
 };
-const imgOptions = {};
+const imgOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px 50px 0px",
+};
 
 const imgObserver = new IntersectionObserver((entries, imgObserver) => {
   entries.forEach((entry) => {
