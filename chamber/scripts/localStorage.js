f502d759376903509ee99a240cc16b250e646a18
Stephanie Clark <sclark@inventoryshield.com>
2022-06-04 19:12:18 -0600

    finishing touches

--- chamber/scripts/localStorage.js
+++ chamber/scripts/localStorage.js
@@ -1,24 +1,39 @@
 const dayVisited = new Date();
 
-let numVisits = Number(window.localStorage.getItem("visits-ls"));
-let lastDayVisited = localStorage.getItem("lastDayVisited");
+let lastDayVisited;
+let timeDifference;
 
-const options = {
-  weekday: "long",
-  year: "numeric",
-  month: "long",
-  day: "numeric",
-};
-const today = dayVisited.toLocaleDateString("en-uk", options);
-
-if (numVisits == false) {
-  localStorage.setItem("visits-ls", "0");
+if (!lastDayVisited) {
+  localStorage.setItem("lastDayVisited", dayVisited);
 } else {
-  numVisits++;
+  lastDayVisited = localStorage.getItem("lastDayVisited");
+  localStorage.setItem("lastDayVisited", dayVisited);
+  timeDifference = difference(lastDayVisited, dayVisited);
 }
 
-if (lastDayVisited == false) {
-  localStorage.setItem("lastDayVisited", "0");
-} else {
-  localStorage.setItem("lastDayVisited", today);
+// I found this function online when researching an appropriate way to get the date difference.
+// I wasn't looking for a function, just an explanation but I researched it and I understand it.
+function difference(date1, date2) {
+  const date1utc = Date.UTC(
+    date1.getFullYear(),
+    date1.getMonth(),
+    date1.getDate()
+  );
+  const date2utc = Date.UTC(
+    date2.getFullYear(),
+    date2.getMonth(),
+    date2.getDate()
+  );
+  const day = 1000 * 60 * 60 * 24;
+  return (date2utc - date1utc) / day;
 }
+
+if (!timeDifference) {
+  timeDifference = 0;
+  document.getElementById("numDaysLastVisit").textContent =
+    "- Today is your first visit!";
+} else {
+  document.getElementById("numDaysLastVisit").textContent = timeDifference;
+}
+
+localStorage.setItem("numberOfDaysSinceLastVisit", timeDifference);
