16cc1ef9c3711e0fbe12b116728152687d1becf9
Stephanie Clark <sclark@inventoryshield.com>
2022-06-16 21:11:56 -0600

    build the script to run the json

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,30 @@
+const requestURL = "https://slclark4.github.io/wdd230/chamber/json/data.json";
+const cards = document.querySelector(".cards");
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject); // temporary checking for valid response and data parsing
+    const businesses = jsonObject["businesses"];
+    businesses.forEach(displayBusinesses);
+  });
+
+function displayBusinesses(business) {
+  let card = document.createElement("section");
+  let h4 = document.createElement("h4");
+  let address = document.createElement("p");
+  let phone = document.createElement("p");
+  let website = document.createElement("a");
+
+  h4.textContent = business.name;
+  address.textContent = business.address;
+  phone.textContent = business.phoneNumber;
+  website.textContent = business.website;
+
+  card.appendChild(h4);
+  card.appendChild(address);
+  card.appendChild(phone);
+  card.appendChild(website);
+}
