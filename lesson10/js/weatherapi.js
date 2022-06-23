670b75b849947c7509763a57a77d142a3c80de0c
Stephanie Clark <sclark@inventoryshield.com>
2022-06-23 15:05:25 -0600

    lesson 10 activity

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,41 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?id=524901&q=Fairbanks&units=imperial&appid=90c455cc6f86cf3c68ec853f22214662";
+
+// dotenv/config
+function displayResults(weatherData) {
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(
+    0
+  )}</strong>`;
+
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
+  const bodyDesc = desc.slice(1);
+  const slicedDesc = desc.slice(0, 1).toUpperCase();
+  const capitalizedDesc = slicedDesc + bodyDesc;
+
+  weatherIcon.setAttribute("src", iconsrc);
+  weatherIcon.setAttribute("alt", capitalizedDesc);
+  captionDesc.textContent = capitalizedDesc;
+}
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log("Data: ", data); // this is for testing the call
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+apiFetch();
