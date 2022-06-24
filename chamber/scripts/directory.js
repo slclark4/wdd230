11214c485438a34225fb2db497cae00b5fbe0edb
Stephanie Clark <sclark@inventoryshield.com>
2022-06-24 08:40:19 -0600

    fix `directory.js` to call and set up the data in a list and cards to be toggled

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,17 +1,20 @@
 const requestURL = "https://slclark4.github.io/wdd230/chamber/json/data.json";
 const cards = document.querySelector(".cards");
+const listItems = document.querySelector(".directory-list");
 
-fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    console.table(jsonObject); // temporary checking for valid response and data parsing
-    const businesses = jsonObject["businesses"];
-    businesses.forEach(displayBusinesses);
-  });
+function callFetch(displayType) {
+  fetch(requestURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (jsonObject) {
+      console.table(jsonObject); // temporary checking for valid response and data parsing
+      const businesses = jsonObject["businesses"];
+      businesses.forEach(displayType);
+    });
+}
 
-function displayBusinesses(business) {
+function displayCardBusinesses(business) {
   let card = document.createElement("section");
   let h3 = document.createElement("h3");
   let address = document.createElement("p");
@@ -35,3 +38,33 @@ function displayBusinesses(business) {
 
   document.querySelector("div.cards").appendChild(card);
 }
+
+function displayListBusinesses(business) {
+  let list = document.createElement("ul");
+  let li = document.createElement("li");
+  let h3 = document.createElement("h3");
+  let address = document.createElement("p");
+  let phone = document.createElement("p");
+  let website = document.createElement("a");
+
+  h3.textContent = business.name;
+  address.textContent = business.address;
+  phone.textContent = business.phoneNumber;
+  website.textContent = business.website;
+  website.setAttribute("href", business.website);
+
+  if (business.website.toLowerCase() === "no website") {
+    website.removeAttribute("href");
+  }
+
+  li.appendChild(h3);
+  li.appendChild(address);
+  li.appendChild(phone);
+  li.appendChild(website);
+  list.appendChild(li);
+
+  document.querySelector("div.directory-list").appendChild(list);
+}
+
+callFetch(displayCardBusinesses);
+callFetch(displayListBusinesses);
